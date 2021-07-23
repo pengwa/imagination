3eed6fc04fe78341500b81522a77f463b0f648e2
pengwa <pengwa@microsoft.com>
2021-07-24 00:26:15 +0800

    Create save_graph_in_run.cc

---
+++ onnxruntime/save_graph_in_run.cc
@@ -0,0 +1,12 @@
+  // static int a = 0;
+  // if (a == 0) {
+  //   std::string file_name = "inference_before_first_run.onnx";
+  //   const std::string target_path = ".";
+  //   if (!target_path.empty()) {
+  //     PathString path_str(target_path + "/" + file_name + ".onnx");
+  //     std::remove(ToMBString(path_str).c_str());
+  //     Model::Save(*model_, path_str);
+  //   }
+
+  //   a += 1;
+  // }
