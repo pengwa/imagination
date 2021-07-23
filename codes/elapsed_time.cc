815754f345a79f96407513ef001f043735ca5cc8
pengwa <pengwa@microsoft.com>
2021-07-24 00:44:59 +0800

    Update elapsed_time.cc

--- codes/elapsed_time.cc
+++ codes/elapsed_time.cc
@@ -1,10 +1,10 @@
-#include
-
+#include <chrono>
+#include <iostream>
 
 auto t0 = std::chrono::high_resolution_clock::now();
 ...
 auto t1 = std::chrono::high_resolution_clock::now();
 std::chrono::duration<float> fs = t1 - t0;
 // std::chrono::milliseconds d = std::chrono::duration_cast<ms>(fs);
-std::cout << "PythonOp e2e latency: " << fs.count() << std::endl;
+std::cout << "PythonOp e2e latency(s): " << fs.count() << std::endl;
 // std::cout << d.count() << "ms\n";
