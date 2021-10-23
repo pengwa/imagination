65b37ec0bca45e8df20e295093d281fe3d76129b
pengwa <pengwa@microsoft.com>
2021-10-23 16:35:13 +0800

    Update torch_extention_utils.cc

--- codes/torch_extention_utils.cc
+++ codes/torch_extention_utils.cc
@@ -1,24 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
 #include <torch/extension.h>
-#include <chrono>
-#include <iostream>
-// #include <omp.h>
-
-#define CHUNK 16
-#define THREADS 8
-
-// #define _OPENMP
-// #include <ATen/ParallelOpenMP.h>
-
-// Don't forget this
-// #include <pybind11/stl_bind.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+#include <pybind11/pytypes.h>
 #include <vector>
-PYBIND11_MAKE_OPAQUE(std::vector<at::Tensor>);
+// PYBIND11_MAKE_OPAQUE(std::vector<OrtValue>);
+// PYBIND11_MAKE_OPAQUE(std::vector<std::vector<at::Tensor>>);
+PYBIND11_MAKE_OPAQUE(std::vector<at::Tensor*>);
 // This function is adapted from microsoft/DeepSpeed fused_adam_frontend.cpp
 void multi_tensor_adam_cuda(int chunk_size,
                             at::Tensor noop_flag,
-                            std::vector<std::vector<at::Tensor>> tensor_lists,
+                            std::vector<at::Tensor>& g,
+                            std::vector<at::Tensor>& p,
+                            std::vector<at::Tensor>& m,
+                            std::vector<at::Tensor>& v,
+                            // std::vector<std::vector<at::Tensor>> tensor_lists,
                             const float lr,
                             const float beta1,
                             const float beta2,
@@ -47,28 +44,6 @@ void multi_tensor_axpby_cuda(int chunk_size,
 
 const int fixed_chunk_size = 2048 * 32;
 
-/*
-class MemoryBuffer:
-    def __init__(self, numel, dtype):
-        self.numel = numel
-        self.dtype = dtype
-        self.data = torch.empty(self.numel,
-                                dtype=self.dtype,
-                                device=torch.cuda.current_device(),
-                                requires_grad=False)
-
-    def get(self, shape, start_index):
-        """Return a tensor with the input `shape` as a view into the
-        1-D data starting at `start_index`."""
-        end_index = start_index + shape.numel()
-        assert end_index <= self.numel, \
-            'requested tensor is out of the buffer range.'
-        buffer_tensor = self.data[start_index:end_index]
-        buffer_tensor = buffer_tensor.view(shape)
-        return buffer_tensor
-
-*/
-
 class MemoryBuffer {
     public:
         MemoryBuffer(size_t numel, at::Tensor val){
@@ -84,11 +59,18 @@ class MemoryBuffer {
         at::Tensor data_buffer_;
 };
 
+class MyClass {
+    public:
+    std::vector<at::Tensor> contents;
+};
 
 // This function is trying to move into C++ implementation from Python logic
 // https://github.com/NVIDIA/apex/blob/0c7d8e3fa9a095a1641a2290877436d0314b69c6/apex/amp/_process_optimizer.py#L161.
 // This would reduce the overhead of long loops.
-void unscale_fp16_grads_into_fp32_grads(std::vector<at::Tensor>& all_fp16_params, 
+// void unscale_fp16_grads_into_fp32_grads(MyClass fp16_cls, MyClass fp32_from_fp16_cls,
+//                                         at::Tensor is_overflow_buffer,
+//                                         float scale) {
+void unscale_fp16_grads_into_fp32_grads(std::vector<at::Tensor>& all_fp16_params,
                                         std::vector<at::Tensor>& all_fp32_from_fp16_params,
                                         at::Tensor is_overflow_buffer,
                                         float scale) {
@@ -102,8 +84,8 @@ void unscale_fp16_grads_into_fp32_grads(std::vector<at::Tensor>& all_fp16_params
 
     std::vector<at::Tensor> fp32_from_fp16_params;
     std::vector<size_t> offsets_mapping;
-    size_t fo32_from_fp16_param_buffer_size = 0;
-    auto t0 = std::chrono::high_resolution_clock::now();
+    size_t fp32_from_fp16_param_buffer_size = 0;
+
     for (size_t i = 0; i < all_fp16_params.size(); ++i) {
         auto& fp16_param_grad = all_fp16_params[i].grad();
         bool fp16_param_has_grad = fp16_param_grad.defined();
@@ -114,51 +96,23 @@ void unscale_fp16_grads_into_fp32_grads(std::vector<at::Tensor>& all_fp16_params
 
         if (fp16_param_has_grad && !fp32_from_fp16_param_has_grad) {
             fp32_from_fp16_params.emplace_back(fp32_from_fp16_param);
-            // fp32_from_fp16_param.mutable_grad() = at::empty_like(fp32_from_fp16_param);
             fp16_grads_needing_unscale.emplace_back(fp16_param_grad);
-            offsets_mapping.emplace_back(fo32_from_fp16_param_buffer_size);
-            fo32_from_fp16_param_buffer_size += fp32_from_fp16_param.numel();
-            // new_fp32_grads.emplace_back(fp32_from_fp16_param.grad());
+            offsets_mapping.emplace_back(fp32_from_fp16_param_buffer_size);
+            fp32_from_fp16_param_buffer_size += fp32_from_fp16_param.numel();
         } else if (fp16_param_has_grad && fp32_from_fp16_param_has_grad) {
             fp16_grads_needing_unscale_with_stash.emplace_back(fp16_param_grad);
             preexisting_fp32_grads.emplace_back(fp32_from_fp16_param_grad);
         }
     }
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> fs = t1 - t0;
 
+    auto mem_buffer = MemoryBuffer(fp32_from_fp16_param_buffer_size, fp32_from_fp16_params[0]);
 
-    auto mem_buffer = MemoryBuffer(fo32_from_fp16_param_buffer_size, fp32_from_fp16_params[0]);
-
-    auto t3 = std::chrono::high_resolution_clock::now();
     new_fp32_grads.resize(fp32_from_fp16_params.size());
-    //##ifdef _OPENMP
-    //##pragma omp parallel for num_threads(THREADS)
-    //##endif
-    // at::parallel_for(0, fp32_from_fp16_params.size(), 0, [&](int64_t start, int64_t end) {
-    //     for (int64_t i = start; i < end; i++)
-    //     {   
-    //         // std::cout << "hi there from " << omp_get_thread_num() << ", handling i: " << i << std::endl;
-    //         fp32_from_fp16_params[i].mutable_grad() = mem_buffer.Get(fp32_from_fp16_params[i], offsets_mapping[i]);
-    //         //at::empty_like(fp32_from_fp16_params[i]);
-    //         new_fp32_grads[i] = fp32_from_fp16_params[i].grad();
-    //     }
-    // });
-    //    for (size_t i = 0; i < fp32_from_fp16_params.size(); ++i) {
-    //       std::cout << "omp_get_thread_num( ): " << omp_get_thread_num() << std::endl;
-    //        fp32_from_fp16_params[i].mutable_grad() = at::empty_like(fp32_from_fp16_params[i]);
-    //        new_fp32_grads[i] = fp32_from_fp16_params[i].grad();
-    //    }
-       for (size_t i = 0; i < fp32_from_fp16_params.size(); ++i) {
-            // std::cout << "hi there from " << omp_get_thread_num() << ", handling i: " << i << std::endl;
-            fp32_from_fp16_params[i].mutable_grad() = mem_buffer.Get(fp32_from_fp16_params[i], offsets_mapping[i]);
-            //at::empty_like(fp32_from_fp16_params[i]);
-            new_fp32_grads[i] = fp32_from_fp16_params[i].grad();
-       }
+    for (size_t i = 0; i < fp32_from_fp16_params.size(); ++i) {
+        fp32_from_fp16_params[i].mutable_grad() = mem_buffer.Get(fp32_from_fp16_params[i], offsets_mapping[i]);
+        new_fp32_grads[i] = fp32_from_fp16_params[i].grad();
+    }
 
-    auto t4 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> fs1 = t4 - t3;
-    std::cout << "latency(s): " << fs.count() << ", " << fs1.count() << std::endl;
     if (fp16_grads_needing_unscale.size() > 0) {
         std::vector<std::vector<at::Tensor>> tensor_lists;
         tensor_lists.emplace_back(fp16_grads_needing_unscale);
@@ -176,23 +130,77 @@ void unscale_fp16_grads_into_fp32_grads(std::vector<at::Tensor>& all_fp16_params
 
 };
 
+
+
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-    // py::class_<std::vector<at::Tensor>>(m, "TensorVector")
+    // py::class_<std::vector<std::vector<at::Tensor>>>(m, "IntVectorVector")
     //     .def(py::init<>())
-    //     .def("push_back", [](std::vector<at::Tensor>* v, const at::Tensor& tensor_value) {
-    //         v->push_back(tensor_value);
-    //     })
-    //     .def("__len__", [](const std::vector<at::Tensor>& v) { return v.size(); })
-    //     .def("__iter__", [](const std::vector<at::Tensor>& v) {
-    //         return py::make_iterator(v.cbegin(), v.cend());
+    //     .def("clear", &std::vector<std::vector<at::Tensor>>::clear)
+    //     .def("pop_back", &std::vector<std::vector<at::Tensor>>::pop_back)
+    //     .def("__len__", [](const std::vector<std::vector<at::Tensor>> &v) { return v.size(); })
+    //     .def("__iter__", [](std::vector<std::vector<at::Tensor>> &v) {
+    //         return py::make_iterator(v.begin(), v.end());
     //     }, py::keep_alive<0, 1>())
-    //     .def("__getitem__", [](const std::vector<<at::Tensor>& v, const size_t idx) {
-    //         return v.at(idx);
+    //     .def("extend", [](std::vector<std::vector<at::Tensor>> &v, const std::vector<std::vector<at::Tensor>> &src) {
+    //         v.insert(v.end(), src.begin(), src.end());
+    //     })
+    //     .def(py::init([](const py::iterable &it) {
+    //         auto v = std::unique_ptr<std::vector<std::vector<at::Tensor>>>(new std::vector<std::vector<at::Tensor>>());
+    //         v->reserve(py::len_hint(it));
+    //         for (const py::iterable& hs : it) {
+    //             auto w = std::vector<at::Tensor>();
+    //             w.reserve(py::len_hint(hs));
+    //             for (py::handle h : hs)
+    //                 w.push_back(h.cast<at::Tensor>());
+    //             v->push_back(w);
+    //         }
+    //         return v.release();
+    //     }));
+    // py::bind_vector<std::vector<std::vector<at::Tensor>>>(m, "IntVectorVector");
+    py::class_<std::vector<at::Tensor>>(m, "IntVector")
+        .def(py::init<>())
+        .def("clear", &std::vector<at::Tensor>::clear)
+        .def("pop_back", &std::vector<at::Tensor>::pop_back)
+        .def("__len__", [](const std::vector<at::Tensor> &v) { return v.size(); })
+        .def("__iter__", [](std::vector<at::Tensor> &v) {
+            return py::make_iterator(v.begin(), v.end());
+        }, py::keep_alive<0, 1>())
+        .def("extend", [](std::vector<at::Tensor> &v, const std::vector<at::Tensor> &src) {
+            v.insert(v.end(), src.begin(), src.end());
+        })
+        .def(py::init([](const py::iterable &it) {
+            auto v = std::unique_ptr<std::vector<at::Tensor>>(new std::vector<at::Tensor>());
+            v->reserve(py::len_hint(it));
+            for (py::handle h : it) {
+                v->push_back(h.cast<at::Tensor>());
+            }
+            return v.release();
+        }));
+
+    // Cannot use this because https://github.com/pybind/pybind11/issues/1470
+    // py::bind_vector<std::vector<at::Tensor>>(m, "IntVector");
+
+
+    py::class_<MyClass>(m, "MyClass")
+        .def(py::init<>())
+        .def_readwrite("contents", &MyClass::contents);
+    // py::class_<std::vector<at::Tensor>>(m, "ACTensorVector")
+    //     .def(py::init<>())
+    //     .def("push_back", [](std::vector<at::Tensor>* v, const at::Tensor& ortvalue) {
+    //       v->push_back(ortvalue);
+    //     })
+    //     .def("reserve", [](std::vector<at::Tensor>* v, const size_t len) { v->reserve(len); })
+    //     .def("shrink_to_fit", [](std::vector<at::Tensor>* v) { v->shrink_to_fit(); })
+    //     .def("__len__", [](const std::vector<at::Tensor> &v) { return v.size(); })
+    //     .def("__iter__", [](const std::vector<at::Tensor> &v) {
+    //       return py::make_iterator(v.cbegin(), v.cend());
+    //     }, py::keep_alive<0, 1>())
+    //     .def("__getitem__", [](const std::vector<at::Tensor> &v, const size_t idx) {
+    //       return v.at(idx);
     //     });
-
-    py::bind_vector<std::vector<at::Tensor>>(m, "AAOrtValueVector");
-
+    // py::bind_vector<std::vector<at::Tensor>>(m, "AAOrtValueVector");
     m.def("multi_tensor_adam",
           &multi_tensor_adam_cuda,
           "Compute and apply gradient update to parameters for Adam optimizer");
