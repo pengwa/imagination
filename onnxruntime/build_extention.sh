a7ff4f086a7110db71931a659c9d23deb3f173da
pengwa <pengwa@microsoft.com>
2023-10-31 17:25:09 +0800

    Update build_extention.sh

--- onnxruntime/build_extention.sh
+++ onnxruntime/build_extention.sh
@@ -1,5 +1,13 @@
 wheel_path=`pip show onnxruntime-training | grep -i location | cut  -d" " -f2`
 
+export CUDA_HOME=/usr/local/cuda-11.8
+export CUDNN_HOME=/usr/local/cuda-11.8
+export CUDACXX=$CUDA_HOME/bin/nvcc
+
+export PATH=/opt/openmpi-4.0.4/bin:$PATH
+export LD_LIBRARY_PATH=/opt/openmpi-4.0.4/lib:$LD_LIBRARY_PATH
+export MPI_CXX_INCLUDE_DIRS=/opt/openmpi-4.0.4/include
+
 #rm $wheel_path/onnxruntime/training/ortmodule/torch_cpp_extensions -rf
 rm $wheel_path/onnxruntime/training/ortmodule/torch_cpp_extensions/cpu/torch_interop_utils/*.h
 rm $wheel_path/onnxruntime/training/ortmodule/torch_cpp_extensions/cpu/torch_interop_utils/*.cc
